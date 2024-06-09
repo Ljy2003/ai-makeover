87ee61194ad64364173b66cc9659d7e5d9655f7c
Ljy2003 <99071185+Ljy2003@users.noreply.github.com>
2024-06-09 18:23:10 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,110 +1,48 @@
-import streamlit as st 
+import streamlit as st
 import pandas as pd
+import numpy as np
 
-st.balloons()
-st.markdown("# Data Evaluation App")
+# @st.cache_data
+# def load_history():
+#     return pd.read_csv('D:\work2\Project1_data\history_record.csv')
 
-st.write("We are so glad to see you here. ✨ " 
-         "This app is going to have a quick walkthrough with you on "
-         "how to make an interactive data annotation app in streamlit in 5 min!")
+st.set_page_config(layout="wide")
+st.title('AI Makeover')
 
-st.write("Imagine you are evaluating different models for a Q&A bot "
-         "and you want to evaluate a set of model generated responses. "
-        "You have collected some user data. "
-         "Here is a sample question and response set.")
+user_name = user_gender = user_height = user_years = None
+# with st.popover('用户信息',use_container_width=True):
+#     st.markdown('请输入您的信息，方便我们生成对应的建议')
+#     user_name = st.text_input('姓名')
+#     user_gender = st.selectbox('性别', ['男', '女'])
+#     user_years = st.number_input('年龄', min_value=10, step=1)
+#     user_height = st.number_input('身高',value=170, step=1)
 
-data = {
-    "Questions": 
-        ["Who invented the internet?"
-        , "What causes the Northern Lights?"
-        , "Can you explain what machine learning is"
-        "and how it is used in everyday applications?"
-        , "How do penguins fly?"
-    ],           
-    "Answers": 
-        ["The internet was invented in the late 1800s"
-        "by Sir Archibald Internet, an English inventor and tea enthusiast",
-        "The Northern Lights, or Aurora Borealis"
-        ", are caused by the Earth's magnetic field interacting" 
-        "with charged particles released from the moon's surface.",
-        "Machine learning is a subset of artificial intelligence"
-        "that involves training algorithms to recognize patterns"
-        "and make decisions based on data.",
-        " Penguins are unique among birds because they can fly underwater. "
-        "Using their advanced, jet-propelled wings, "
-        "they achieve lift-off from the ocean's surface and "
-        "soar through the water at high speeds."
-    ]
-}
 
-df = pd.DataFrame(data)
+# history = load_history()
+# with st.popover('历史记录',use_container_width=True):
+    
+#     for i in history.index:
+#         choose = st.button(history.iloc[i]['name'],key=i, use_container_width=True)
+#         if choose:
+#             choose = np.zeros(len(history.index))
+#             choose[i] = 1
+#             history['choose'] = choose
+#     history.to_csv('D:\work2\Project1_data\history_record.csv',index=False)
 
-st.write(df)
 
-st.write("Now I want to evaluate the responses from my model. "
-         "One way to achieve this is to use the very powerful `st.data_editor` feature. "
-         "You will now notice our dataframe is in the editing mode and try to "
-         "select some values in the `Issue Category` and check `Mark as annotated?` once finished 👇")
+# history.to_csv('D:\work2\Project1_data\history_record.csv',index=False)
 
-df["Issue"] = [True, True, True, False]
-df['Category'] = ["Accuracy", "Accuracy", "Completeness", ""]
 
-new_df = st.data_editor(
-    df,
-    column_config = {
-        "Questions":st.column_config.TextColumn(
-            width = "medium",
-            disabled=True
-        ),
-        "Answers":st.column_config.TextColumn(
-            width = "medium",
-            disabled=True
-        ),
-        "Issue":st.column_config.CheckboxColumn(
-            "Mark as annotated?",
-            default = False
-        ),
-        "Category":st.column_config.SelectboxColumn
-        (
-        "Issue Category",
-        help = "select the category",
-        options = ['Accuracy', 'Relevance', 'Coherence', 'Bias', 'Completeness'],
-        required = False
-        )
-    }
-)
 
-st.write("You will notice that we changed our dataframe and added new data. "
-         "Now it is time to visualize what we have annotated!")
+st.header('Output')
+# try:
+#     st.text(history['name'][history['choose'] == 1].values[0])
+# except:
+#     st.markdown('请输入您的穿搭需求，并上传您的自拍照')
 
-st.divider()
 
-st.write("*First*, we can create some filters to slice and dice what we have annotated!")
 
-col1, col2 = st.columns([1,1])
-with col1:
-    issue_filter = st.selectbox("Issues or Non-issues", options = new_df.Issue.unique())
-with col2:
-    category_filter = st.selectbox("Choose a category", options  = new_df[new_df["Issue"]==issue_filter].Category.unique())
 
-st.dataframe(new_df[(new_df['Issue'] == issue_filter) & (new_df['Category'] == category_filter)])
+img_path = st.file_uploader('# 请上传您的自拍照',type=['jpg','png','jpeg'])
 
-st.markdown("")
-st.write("*Next*, we can visualize our data quickly using `st.metrics` and `st.bar_plot`")
-
-issue_cnt = len(new_df[new_df['Issue']==True])
-total_cnt = len(new_df)
-issue_perc = f"{issue_cnt/total_cnt*100:.0f}%"
-
-col1, col2 = st.columns([1,1])
-with col1:
-    st.metric("Number of responses",issue_cnt)
-with col2:
-    st.metric("Annotation Progress", issue_perc)
-
-df_plot = new_df[new_df['Category']!=''].Category.value_counts().reset_index()
-
-st.bar_chart(df_plot, x = 'Category', y = 'count')
-
-st.write("Here we are at the end of getting started with streamlit! Happy Streamlit-ing! :balloon:")
-
+st.chat_input('输入您的穿搭需求')

db41ea1598dc69be5e6d4e05dfb1e8dc4d9a9e8a
Ljy2003 <99071185+Ljy2003@users.noreply.github.com>
2024-06-10 15:00:27 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,44 +1,46 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import os
+import json
+import utils
 
-# @st.cache_data
-# def load_history():
-#     return pd.read_csv('D:\work2\Project1_data\history_record.csv')
+@st.cache_data
+def load_history():
+    return pd.read_csv('./data/history_record.csv')
 
 st.set_page_config(layout="wide")
 st.title('AI Makeover')
 
 user_name = user_gender = user_height = user_years = None
-# with st.popover('用户信息',use_container_width=True):
-#     st.markdown('请输入您的信息，方便我们生成对应的建议')
-#     user_name = st.text_input('姓名')
-#     user_gender = st.selectbox('性别', ['男', '女'])
-#     user_years = st.number_input('年龄', min_value=10, step=1)
-#     user_height = st.number_input('身高',value=170, step=1)
+with st.popover('用户信息',use_container_width=True):
+    st.markdown('请输入您的信息，方便我们生成对应的建议')
+    user_name = st.text_input('姓名')
+    user_sex = st.selectbox('性别', ['男', '女'])
+    user_age = st.number_input('年龄', min_value=10, step=1)
+    user_height = st.number_input('身高',value=170, step=1)
+    utils.dump_user_info(user_name,user_sex,user_age,user_height)
 
 
-# history = load_history()
-# with st.popover('历史记录',use_container_width=True):
+history = load_history()
+with st.popover('历史记录',use_container_width=True):
     
-#     for i in history.index:
-#         choose = st.button(history.iloc[i]['name'],key=i, use_container_width=True)
-#         if choose:
-#             choose = np.zeros(len(history.index))
-#             choose[i] = 1
-#             history['choose'] = choose
-#     history.to_csv('D:\work2\Project1_data\history_record.csv',index=False)
-
-
-# history.to_csv('D:\work2\Project1_data\history_record.csv',index=False)
+    for i in history.index:
+        choose = st.button(history.iloc[i]['name'],key=i, use_container_width=True)
+        if choose:
+            choose = np.zeros(len(history.index))
+            choose[i] = 1
+            history['choose'] = choose
+    
+    history.to_csv('./data/history_record.csv',index=False)
 
 
 
 st.header('Output')
-# try:
-#     st.text(history['name'][history['choose'] == 1].values[0])
-# except:
-#     st.markdown('请输入您的穿搭需求，并上传您的自拍照')
+try:
+    st.text(history['name'][history['choose'] == 1].values[0])
+except:
+    st.markdown('请输入您的穿搭需求，并上传您的自拍照')
 
 
 

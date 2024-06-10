8848fefa555a9aa6a890536a6be7621f1afb977d
Ljy2003 <99071185+Ljy2003@users.noreply.github.com>
2024-06-10 15:36:21 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,18 +1,31 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
+import os
+import json
+import utils
 
-@st.experimental_dialog("Cast your vote")
-def vote(item):
-    st.write(f"Why is {item} your favorite?")
-    reason = st.text_input("Because...")
-    if st.button("Submit"):
-        st.session_state.vote = {"item": item, "reason": reason}
-        st.rerun()
+@st.cache_data
+def load_history():
+    return pd.read_csv('./data/history_record.csv')
 
-if "vote" not in st.session_state:
-    st.write("Vote for your favorite")
-    if st.button("A"):
-        vote("A")
-    if st.button("B"):
-        vote("B")
-else:
-    f"You voted for {st.session_state.vote['item']} because {st.session_state.vote['reason']}"
+# st.set_page_config(layout="wide")
+st.title('AI Makeover')
+
+user_name = user_gender = user_height = user_years = None
+
+if st.button('用户信息',use_container_width=True):
+    utils.user_info()
+
+if st.button('历史记录',use_container_width=True):
+    utils.load_history()
+
+st.header('Output')
+st.markdown('请输入您的穿搭需求，并上传您的自拍照')
+
+
+
+
+img_path = st.file_uploader('# 请上传您的自拍照',type=['jpg','png','jpeg'])
+
+st.chat_input('输入您的穿搭需求')

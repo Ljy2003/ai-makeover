cea035e044ca6f9a9957ff5b92b52452f36e701e
Ljy2003 <99071185+Ljy2003@users.noreply.github.com>
2024-06-10 15:10:06 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,10 @@
-
 import streamlit as st
 
-with st.popover("Open popover"):
-    st.markdown("Hello World 👋")
-    name = st.text_input("What's your name?")
+popover = st.popover("Filter items")
+red = popover.checkbox("Show red items.", True)
+blue = popover.checkbox("Show blue items.", True)
 
-st.write("Your name:", name)
+if red:
+    st.write(":red[This is a red item.]")
+if blue:
+    st.write(":blue[This is a blue item.]")

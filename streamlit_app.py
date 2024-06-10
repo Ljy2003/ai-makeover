0ba9bc5a48a7225914e6afad9215e5bad9fe42b2
Ljy2003 <99071185+Ljy2003@users.noreply.github.com>
2024-06-10 15:25:21 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,18 @@
 import streamlit as st
 
-popover = st.popover("Filter items")
-red = popover.checkbox("Show red items.", True)
-blue = popover.checkbox("Show blue items.", True)
+@st.experimental_dialog("Cast your vote")
+def vote(item):
+    st.write(f"Why is {item} your favorite?")
+    reason = st.text_input("Because...")
+    if st.button("Submit"):
+        st.session_state.vote = {"item": item, "reason": reason}
+        st.rerun()
 
-if red:
-    st.write(":red[This is a red item.]")
-if blue:
-    st.write(":blue[This is a blue item.]")
+if "vote" not in st.session_state:
+    st.write("Vote for your favorite")
+    if st.button("A"):
+        vote("A")
+    if st.button("B"):
+        vote("B")
+else:
+    f"You voted for {st.session_state.vote['item']} because {st.session_state.vote['reason']}"

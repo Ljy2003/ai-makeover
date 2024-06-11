1ebdcb45148a34b44da42ff7f89791c0f7a5a4c4
Ljy2003 <99071185+Ljy2003@users.noreply.github.com>
2024-06-11 17:19:38 +0800

    Delete menu.py

--- menu.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-def menu():
-    if 'loc' not in st.session_state.keys():
-        import streamlit_app
-    else:
-        st.sidebar.page_link('streamlit_app.py',label='首页')
-        st.sidebar.page_link('./pages/chat_page.py',label='穿搭推荐')
-        st.sidebar.page_link('./pages/preview_page.py',label='穿搭预览')

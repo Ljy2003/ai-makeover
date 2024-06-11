c4a84721f2a538781779f26a81b1a848f95c23ff
Ljy2003 <99071185+Ljy2003@users.noreply.github.com>
2024-06-11 16:49:39 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,31 +1,25 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
-import os
-import json
-import utils
+from menu import menu
 
-@st.cache_data
-def load_history():
-    return pd.read_csv('./data/history_record.csv')
+st.session_state['loc'] = 'main'
 
-# st.set_page_config(layout="wide")
 st.title('AI Makeover')
-
-user_name = user_gender = user_height = user_years = None
-
-if st.button('用户信息',use_container_width=True):
-    utils.user_info()
-
-if st.button('历史记录',use_container_width=True):
-    utils.load_history()
-
-st.header('Output')
-st.markdown('请输入您的穿搭需求，并上传您的自拍照')
-
-
-
-
-img_path = st.file_uploader('# 请上传您的自拍照',type=['jpg','png','jpeg'])
-
-st.chat_input('输入您的穿搭需求')
+st.markdown('您的智能穿搭助手')
+with st.popover('点击查看详情',use_container_width=True):
+    st.markdown(
+        '''
+        AI Makeover是一款基于多模态大模型的智能穿搭工具
+        '''
+    )
+with st.popover('使用说明',use_container_width=True):
+    st.markdown(
+        '''
+        * 在侧边栏点击 **穿搭推荐** ，上传您的自拍照（全身或半身），
+        随后输入您的穿搭需求（如：我想要干净清爽/柔和舒适的穿搭）
+        * 在穿搭推荐界面中，点击 **生成穿搭建议**，得到个性化的穿搭建议
+        * 在穿搭推荐界面中，点击 **查看推荐潮流单品**，挑选合适的推荐单品
+        * 
+        '''
+    )
+st.image('fig/logo.png')
+menu()

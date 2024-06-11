523e4c6e2ddca1c3efe7ab639dc4bf9ba77f30f8
Ljy2003 <99071185+Ljy2003@users.noreply.github.com>
2024-06-11 17:24:55 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,8 @@
 import streamlit as st
 st.title('AI Makeover')
+with st.popover('点击查看详情',use_container_width=True):
+    st.markdown(
+        '''
+        AI Makeover是一款基于多模态大模型的智能穿搭工具
+        '''
+    )

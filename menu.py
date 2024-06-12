e9e2ae6fb9a4ccd9a3986408c4557a62d33e59d9
Ljy2003 <99071185+Ljy2003@users.noreply.github.com>
2024-06-12 16:58:09 +0800

    Add files via upload

--- menu.py
+++ menu.py
@@ -5,5 +5,5 @@ def menu():
         import streamlit_app
     else:
         st.sidebar.page_link('streamlit_app.py',label='首页')
-        st.sidebar.page_link('./pages/chat_page.py',label='穿搭推荐')
+        st.sidebar.page_link('./pages/chat_page.py',label='AI推荐')
         st.sidebar.page_link('./pages/preview_page.py',label='穿搭预览')
--- pages/chat_page.py
+++ pages/chat_page.py
@@ -1,12 +1,27 @@
 import streamlit as st
 from menu import menu
+import cv2
+import PIL
+from PIL import Image
+from io import BytesIO
 
 st.session_state['loc'] = 'chat'
-st.markdown('### 请输入您的穿搭需求，并上传您的自拍照，我们将会给您提供智能穿搭推荐')
+st.markdown('### 请输入您的穿搭需求，并上传您的自拍照，我们将会为您提供智能穿搭推荐')
 
-st.markdown('**请上传您的自拍照**')
-st.file_uploader('',type=['png','jpg','jpeg'])
+col1,col2 = st.columns(2)
+with col1:
+    st.button('生成穿搭建议',use_container_width=True)
+with col2:
+    st.button('查看推荐潮流单品',use_container_width=True)
+
+
+file = st.file_uploader('**请上传您的自拍照**',type=['png','jpg','jpeg'])
+if file is not None:
+    st.image(file,caption='自拍',use_column_width=True)
+    bytes_data = file.getvalue()
+    bytes_stream = BytesIO(bytes_data)
+    img = Image.open(bytes_stream)
+    img.save('./data/images/selfie//selfie.png')
+
 st.chat_input('请输入您的穿搭需求')
-st.button('生成穿搭建议',use_container_width=True)
-st.button('查看推荐潮流单品',use_container_width=True)
 menu()

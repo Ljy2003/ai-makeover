cd917f15fc2cab8019fde3f7caad5265179d8e83
Ljy2003 <99071185+Ljy2003@users.noreply.github.com>
2024-06-13 16:57:08 +0800

    Update chat_page.py

--- pages/chat_page.py
+++ pages/chat_page.py
@@ -4,20 +4,17 @@ from PIL import Image
 from io import BytesIO
 
 st.set_page_config(layout='wide')
+
 st.image('./fig/chat_title.png')
 
-col1,col2 = st.columns(2)
-flag = col1.button('输入需求',type='primary',use_container_width=True)
 with open('data/need.txt','r',encoding='utf-8') as f:
     needs = f.readline()
-
 text = st.text_input('请输入您的穿搭需求',needs)
+flag = st.button('输入需求',use_container_width=True)
 if flag:
     with open('data/need.txt','w',encoding='utf-8') as f:
         f.write(text)
     st.markdown('已上传您的穿搭需求')
-if col2.button('一键改造',type='primary',use_container_width=True):
-    pass
 
 file = st.file_uploader('**请上传您的自拍照**',type=['png','jpg','jpeg'])
 if file is not None:
@@ -27,5 +24,7 @@ if file is not None:
     img = Image.open(bytes_stream)
     img.save('./data/images/selfie//selfie.png')
 
-if st.button('返回',type='primary',use_container_width=True):
+if st.button('一键改造',type='primary',use_container_width=True):
+    pass
+if st.button('返回',use_container_width=True):
     st.switch_page('streamlit_app.py')
